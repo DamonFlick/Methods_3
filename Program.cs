0ed33b9c4fa21f5fb0fbd0d10064702f29d672fa
DamonFlick <75501983+DamonFlick@users.noreply.github.com>
2021-02-23 19:47:45 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter two numbers, one after the other and witness some sweet addition. You can also omit the second number.");
+            int x = Convert.ToInt32(Console.ReadLine());
+            int y;
+            string optInput = Console.ReadLine();
+            if ( String.IsNullOrEmpty(optInput))
+            {
+
+                Console.WriteLine("Result: " + operations.expression(x));
+            }
+            else
+            {
+                y = Convert.ToInt32(optInput
+                    );
+                Console.WriteLine("Result: " + operations.expression(x, y));
+            }
+            
+            Console.ReadLine();
+        }
+    }
+}
---
+++ operations.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods_3
+{
+    class operations
+    {
+        public static int expression(int x ,int  y = 6) 
+        {
+            return x + y;
+        }
+    }
+}
